9e5b2bf5b1ebcb1da37159c48e5e5c80cd6422a6
root <root@localhost.localdomain>
2023-11-09 15:51:13 +0800

    2 commit

--- customlogger/classes/logmanager.php
+++ customlogger/classes/logmanager.php
@@ -4,12 +4,15 @@ namespace local_customlogger;
 defined('MOODLE_INTERNAL') || die();
 class logmanager {
     public static function log_submission_created($event) {
-        // 獲取課程和學生信息
-        $courseid = $event->courseid;
-        $userid = $event->userid;
+        // 獲取學生ID和事件資料陣列
+    $userid = $event->userid;
+    $data = $event->get_data();
 
-        // 寫入 error.log 檔案
-        $logmessage = "Student {$userid} created an assignment on course {$courseid}.";
-        error_log($logmessage);
+    // 轉換陣列成字串，使用print_r函數
+    $dataString = print_r($data, true);
+
+    // 寫入error.log檔案
+    $logmessage = "User ID: {$userid}, Event Data: {$dataString}";
+    error_log($logmessage);
     }
 }
--- customlogger/version.php
+++ customlogger/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023110904; // 插件的版本號，請根據需要更新
+$plugin->version   = 2023110905; // 插件的版本號，請根據需要更新
 $plugin->requires  = 2019111200; // 適用的 Moodle 最低版本號
 $plugin->component = 'local_customlogger'; // 插件的 component 名稱，需要與資料夾名稱一致
